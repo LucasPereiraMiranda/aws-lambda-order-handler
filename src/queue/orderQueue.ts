a0b5f5a66c11d50b47ad9c78bbc8716dc9f714a8
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-02-24 18:12:36 -0300

    feat: create order queue

---
+++ src/queue/orderQueue.ts
@@ -0,0 +1,23 @@
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+import { Order } from '@/entities/order';
+
+export class OrderQueue {
+  private sqsClient: SQSClient;
+  private queueUrl: string;
+
+  constructor(sqsClient: SQSClient, queueUrl: string) {
+    this.sqsClient = sqsClient;
+    this.queueUrl = queueUrl;
+  }
+
+  async enqueue(order: Order): Promise<string> {
+    const command = new SendMessageCommand({
+      QueueUrl: this.queueUrl,
+      MessageBody: JSON.stringify(order),
+    });
+
+    const response = await this.sqsClient.send(command);
+    console.log('Message sent successfully:', response.MessageId);
+    return response.MessageId!;
+  }
+}
