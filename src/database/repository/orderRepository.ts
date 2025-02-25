bdd273edd98df6cb1ee710c51e43a75aec9505ed
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-02-25 09:00:00 -0300

    refactor: create interface

--- src/database/repository/orderRepository.ts
+++ src/database/repository/orderRepository.ts
@@ -1,13 +1,14 @@
 import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
-import { Order } from '@/entities/order';
+import { Order } from '@/entities/order/order.type';
+import { IRepository } from '@/database/repository/interface/repository.interface';
 
-export class OrderRepository {
+export class OrderRepository implements IRepository<Order> {
   private dynamoDbClient: DynamoDBClient;
   private tableName: string;
 
-  constructor(dynamoDbClient: DynamoDBClient, tableName: string) {
+  constructor(dynamoDbClient: DynamoDBClient) {
     this.dynamoDbClient = dynamoDbClient;
-    this.tableName = tableName;
+    this.tableName = process.env.ORDERS_TABLE || 'OrdersTable-dev';
   }
 
   async save(order: Order): Promise<void> {
